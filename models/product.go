bd8bd12c3612e0023c051e952320d1510edd4ab4
ferueda <fe.rueda@outlook.com>
2022-01-18 15:33:28 -0800

    Add basic models logic

--- models/product.go
+++ models/product.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -19,13 +20,49 @@ type Product struct {
 
 type Products []Product
 
-func (p Products) ToJSON(w io.Writer) error {
+func (pp *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
-	return e.Encode(p)
+	return e.Encode(&pp)
+}
+
+func (p *Product) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(&p)
 }
 
 func GetProducts() Products {
 	return productList
+}
+
+func AddProduct(p Product) {
+	p.ID = getNextId()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p Product) error {
+	p.ID = id
+	idx, err := findProductIdx(id)
+	if err != nil {
+		return err
+	}
+
+	productList[idx] = p
+	return nil
+}
+
+func getNextId() int {
+	return productList[len(productList)-1].ID + 1
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProductIdx(id int) (int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return i, nil
+		}
+	}
+	return -1, ErrProductNotFound
 }
 
 var productList = []Product{
