dc094c5c1aef352bb6904cc3a0f3b68b5a8d6fde
ferueda <fe.rueda@outlook.com>
2022-01-17 23:09:24 -0800

    Create products model

---
+++ models/product.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+type Product struct {
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedAt   string  `json:"-"`
+	UpdatedAt   string  `json:"-"`
+	DeletedAt   string  `json:"-"`
+}
+
+type Products []Product
+
+func (p Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func GetProducts() Products {
+	return productList
+}
+
+var productList = []Product{
+	Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffe",
+		Price:       2.45,
+		SKU:         "abc123",
+		CreatedAt:   time.Now().UTC().String(),
+		UpdatedAt:   time.Now().UTC().String(),
+	},
+	Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "Another description",
+		CreatedAt:   time.Now().UTC().String(),
+		UpdatedAt:   time.Now().UTC().String(),
+	},
+}
